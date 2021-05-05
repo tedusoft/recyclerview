ef0ba66ec09922644e353081940f00f923f5383c
Cristopher Salazar <salazar6cristopehr@gmail.com>
2021-05-04 22:36:02 -0500

    Test 02

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import "./index.scss";
-import { BrowserRouter as Router } from "react-router-dom";
+import { HashRouter as Router } from "react-router-dom";
 ReactDOM.render(
 	<React.StrictMode>
 		<Router>
